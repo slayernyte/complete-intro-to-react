34779a71ae69114c95cbe1584a221ca3bccdb191
thecodegoddess <thecodegodess17@gmail.com>
2016-11-15 21:17:48 -0800

    added notes, fixed error

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,34 @@
 const path = require('path')
 
 module.exports = {
+  // node current directory, context tell webpack where to run everything from
   context: __dirname,
+  // main file for the project
   entry: './js/ClientApp.js',
+
   output: {
+    // where to put the bundle
     path: path.join(__dirname, '/public'),
+    // name of the bundled file
     filename: 'bundle.js'
   },
   resolve: {
-    extentions: ['', '.js', '.jsx', '.json']
+    // in order, require resolve extentions to look for
+    extensions: ['', '.js', '.jsx', '.json']
   },
   stats: {
-    colors: true,
+    colors: true, // show colors
     reasons: true, // verbose messaging
-    chunks : false
+    chunks : false // TODO
   },
+  // external module that webpack should use
   module: {
+    // array of extra build steps, build pipeline
     loaders: [
       {
+        // if file name passes this, use this loader
         test: /\.jsx?$/,
+        // run it through this
         loader: 'babel-loader'
       }
     ]
