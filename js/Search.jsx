b6d033a71b39e94f10fdfceea12b2d5f7cad6590
thecodegoddess <thecodegodess17@gmail.com>
2016-11-18 07:47:17 -0800

    stopping point

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,16 +2,44 @@ const React = require('react')
 const data = require('../public/data')
 const ShowCard = require('./ShowCard')
 
-const Search = () => (
-  <div className="container">
-    <div className="shows">
-      {data.shows.map((show) => (
-        // spread operator
-        <ShowCard {...show} key={show.imdbID} />
-      ))}
+// This does not have auto-binding unlike react createClass
+// - class Search extends React.Component {
+const Search = React.createClass({
+  getInitialState () {
+    // class syntax, this is done in the instructor
+    return {
+      searchTerm: 'amber'
+    }
+  },
+  // synthetic event
+  handleSearchTermEvent (ev) {
+    this.setState({searchTerm: ev.target.value})
+  },
+  render () {
+    // keypress fires a re-render but with out anything to change the value of the state
+    // nothing changes in the input so value stays the same
+    //TODO: React Link State Google
+    return (
+      <div className="container">
+        <header className="header">
+          <h1 className="brand">svideo {this.state.searchTerm}</h1>
+          <input type="text"
+            className="search-input"
+            placeholder="search"
+            value={this.state.searchTerm}
+            onChange={this.handleSearchTermEvent}
+          />
+        </header>
+        <div className="shows">
+          {data.shows.map((show) => (
+            // spread operator
+            <ShowCard {...show} key={show.imdbID} />
+          ))}
 
-    </div>
-  </div>
-)
+        </div>
+      </div>
+    )
+  }
+})
 
 module.exports = Search
