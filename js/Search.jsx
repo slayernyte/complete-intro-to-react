e36f00aabf4614d0c5abd9ccd205f1ab1de1716d
thecodegoddess <thecodegodess17@gmail.com>
2016-11-19 22:32:53 -0800

    added filter functionality

--- js/Search.jsx
+++ js/Search.jsx
@@ -4,11 +4,13 @@ const ShowCard = require('./ShowCard')
 
 // This does not have auto-binding unlike react createClass
 // - class Search extends React.Component {
+
+// Search is automagicly bound to this for you
 const Search = React.createClass({
   getInitialState () {
     // class syntax, this is done in the instructor
     return {
-      searchTerm: 'amber'
+      searchTerm: ''
     }
   },
   // synthetic event
@@ -18,11 +20,11 @@ const Search = React.createClass({
   render () {
     // keypress fires a re-render but with out anything to change the value of the state
     // nothing changes in the input so value stays the same
-    //TODO: React Link State Google
+    // TODO: React Link State Google
     return (
       <div className="container">
         <header className="header">
-          <h1 className="brand">svideo {this.state.searchTerm}</h1>
+          <h1 className="brand">s video</h1>
           <input type="text"
             className="search-input"
             placeholder="search"
@@ -31,9 +33,18 @@ const Search = React.createClass({
           />
         </header>
         <div className="shows">
-          {data.shows.map((show) => (
+          {data.shows
+          // if the search term is anywhere in my show title or description, then return true
+            // make a string containing the serc title and description
+            // set it to uppercase to normalize
+            // set the search term to uppercase and use it as the argument of indexOf
+            // if the search term is found in the tile + description string, return it
+            .filter((show) => `${show.title} ${show.description}`
+              .toUpperCase()
+              .indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map((show) => (
             // spread operator
-            <ShowCard {...show} key={show.imdbID} />
+              <ShowCard {...show} key={show.imdbID} />
           ))}
 
         </div>
